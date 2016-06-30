3e9e2e3cf670aaf3197668db69b64be57d1349c4
Johan <johan614@gmail.com>
2016-06-30 23:26:58 +0200

    uppdaterade filen

--- gograph.go
+++ gograph.go
@@ -1,7 +1,23 @@
 package main
 
+// make needed imports
 import "fmt"
 
+
+// read settings from settings file
+
+// get data from cucm
+
+// save data to a storage format
+
+// read data from storage format
+
+// read html template
+
+// put data in html files
+
+
+// func main
 func main() {
 	fmt.Printf("Heelloooo")
 }
