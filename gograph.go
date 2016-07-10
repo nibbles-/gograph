a5c23ce63acc7d02b4252e7e7f8db50b175e64c4
Johan <johan614@gmail.com>
2016-07-10 22:48:30 +0200

    gabbagabb

--- gograph.go
+++ gograph.go
@@ -1,23 +1,29 @@
 package main
 
 // make needed imports
-import "fmt"
+import (
+	"fmt"
+	"flag"
+)
 
+var (
+	flag1 = flag.String("flag1", "default1", "Description1")
+	flag2 = flag.Int("flag2", 123, "Description2")
+	flag3 = flag.String("flag3", "Default3", "Description3")
+)
 
-// read settings from settings file
-
-// get data from cucm
-
-// save data to a storage format
-
-// read data from storage format
-
-// read html template
-
-// put data in html files
-
-
-// func main
-func main() {
-	fmt.Printf("Heelloooo")}
+func init(){
+	// read settings from settings file and flags
+	flag.Parse()
+}
+func main(){
+	// get data from cucm
+	// save data to a storage format
+	// read data from storage format
+	// read html template
+	// put data in html files
+	fmt.Println(*flag1)
+	fmt.Println(*flag2)
+	fmt.Println(*flag3)		
+}
 
