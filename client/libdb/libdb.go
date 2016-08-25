be3751863c84b6cea0c545ff44f98c382efef595
johan614@gmail.com <D570030@tdc.se>
2016-08-25 16:30:25 +0200

    asd

--- client/libdb/libdb.go
+++ client/libdb/libdb.go
@@ -11,14 +11,19 @@ type Tick struct {
 	Value     int
 }
 
+type table struct {
+	Name     string
+	Interval int
+	Rows     int
+	Overflow *table
+	Ticks    []Tick
+}
+
 // Database is a type for storing stats in
 type Database struct {
-	name     string
-	interval int
-	rows     int
-	file     string
-	child    *Database
-	ticks    []Tick
+	Name   string
+	File   string
+	Tables []table
 }
 
 // Info prints information about the DB
@@ -27,70 +32,36 @@ func (db *Database) Info() {
 }
 
 // Append an item to the DB according to intervall and row rules
-func (db *Database) Append(ticker Tick) {
-	if len(db.ticks) > 0 {
-		if ticker.Timestamp-db.ticks[len(db.ticks)-1].Timestamp < int64(db.interval) {
-			log.Printf("%v is within the less than %v from last entry. Not added\n", ticker.Timestamp, db.interval)
+func (tbl *table) Append(ticker Tick) {
+	if len(tbl.Ticks) > 0 {
+		if ticker.Timestamp-tbl.Ticks[len(tbl.Ticks)-1].Timestamp < int64(tbl.Interval) {
+			log.Printf("%v is less than %v from last entry. Not added\n", ticker.Timestamp, tbl.Interval)
 		} else {
-			if len(db.ticks) >= db.rows {
-				db.ticks = append(db.ticks[:0], db.ticks[1:]...)
-				db.ticks = append(db.ticks, ticker)
+			if len(tbl.Ticks) >= tbl.Rows {
+				tbl.Ticks = append(tbl.Ticks[:0], tbl.Ticks[1:]...)
+				tbl.Ticks = append(tbl.Ticks, ticker)
 			} else {
-				db.ticks = append(db.ticks, ticker)
+				tbl.Ticks = append(tbl.Ticks, ticker)
 			}
 		}
 	} else {
-		db.ticks = append(db.ticks, ticker)
+		tbl.Ticks = append(tbl.Ticks, ticker)
 	}
 }
 
-// SetName sets the Name of the Database
-func (db *Database) SetName(name string) {
-	db.name = name
-}
-
-// Name returns the name of the Database
-func (db *Database) Name() string {
-	return db.name
-}
-
-// SetRows sets the number of rows the database can handle before overflowing
-func (db *Database) SetRows(rows int) {
-	db.rows = rows
-}
-
-// Rows returns the number of rows the database can handle before overflowing
-func (db *Database) Rows() int {
-	return db.rows
-}
-
-// SetInterval sets the interval in SECONDS between entries in the database.
-// Entries with a lower interval will be ignored
-func (db *Database) SetInterval(interval int) {
-	db.interval = interval
-}
-
-// Interval returns the interval of the database
-func (db *Database) Interval() int {
-	return db.interval
-}
-
-// SetFile sets the path to the file where to store the db
-func (db *Database) SetFile(file string) {
-	db.file = file
-}
-
-// File returns the file-string of the database
-func (db *Database) File() string {
-	return db.file
+// NewTable creates a new table in the Database
+func (db *Database) NewTable(name string, interval int, rows int, overflow *table) *table {
+	tbl := table{Name: name, Interval: interval, Rows: rows, Overflow: overflow}
+	db.Tables = append(db.Tables, tbl)
+	return &tbl
 }
 
 // GetAverage gets the average value of the ticks.Value
-func (db *Database) GetAverage() int {
+func (tbl *table) GetAverage() int {
 	var average int
-	for _, ticker := range db.ticks {
+	for _, ticker := range tbl.Ticks {
 		average += ticker.Value
 	}
-	average = average / len(db.ticks)
+	average = average / len(tbl.Ticks)
 	return average
 }
